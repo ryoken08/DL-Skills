25910c23bcec93def0edaa6bb8b61b9418881d63
ryoken08 <nekushiro@gmail.com>
2021-02-02 00:28:35 -0600

    Added Lua & Luca Skills

---
+++ script/c380000154.lua
@@ -0,0 +1,35 @@
+--Equipment Exchange
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--opd check
+	if Duel.GetFlagEffect(ep,id)>0 then return end
+	--condition
+	return aux.CanActivateSkill(tp)
+	and Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_HAND,0,1,nil,TYPE_EQUIP)
+	--and Duel.GetLP(tp)<=3000
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	--opd register
+	Duel.RegisterFlagEffect(ep,id,0,0,0)
+	local ct=1
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,Card.IsType,tp,LOCATION_HAND,0,1,1,nil,TYPE_EQUIP)
+	if #g>0 then
+		Duel.SendtoDeck(g,nil,SEQ_DECKBOTTOM,REASON_RULE)
+	end
+	if Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+		local sg=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_HAND,0,1,1,nil)
+		if #sg>0 then
+			Duel.SendtoDeck(sg,nil,SEQ_DECKBOTTOM,REASON_RULE)
+			ct=2
+		end
+	end
+	Duel.BreakEffect()
+	Duel.Draw(tp,ct,REASON_RULE)
+end
---
+++ script/c380000157.lua
@@ -0,0 +1,26 @@
+--Field Restock
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--opd check
+	if Duel.GetFlagEffect(ep,id)>0 then return end
+	--condition
+	return aux.CanActivateSkill(tp)
+	and Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_GRAVE,0,3,nil,TYPE_FIELD)
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	--opd register
+	Duel.RegisterFlagEffect(ep,id,0,0,0)
+	local g=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_GRAVE,0,nil,TYPE_FIELD):RandomSelect(tp,1)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_RULE)
+	end
+	local sg=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_GRAVE,0,nil,TYPE_FIELD)
+	if #sg>0 then
+		Duel.SendtoDeck(sg,nil,SEQ_DECKSHUFFLE,REASON_RULE)
+	end
+end

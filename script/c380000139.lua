8ff3af839aa2e82adfa26cc236830a33e4f0f24a
ryoken08 <nekushiro@gmail.com>
2021-02-06 01:31:12 -0600

    Added Majestic Skills
    - Added missing string for Shell of a Ghost
    - Added Majestic Light and Majestic Guardian

---
+++ script/c380000139.lua
@@ -0,0 +1,45 @@
+--Majestic Light
+local s,id=GetID()
+function s.initial_effect(c)
+	--skill
+	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop,1)
+end
+s.listed_names={21159309}
+function s.filter(c)
+	return c:IsType(TYPE_TUNER)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--condition
+	return aux.CanActivateSkill(tp)
+	and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil)
+	and Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_DECK,0,1,nil,21159309)
+	and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil)
+	if #g>0 then
+		Duel.ConfirmCards(1-tp,g)
+		Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_RULE)
+		Duel.BreakEffect()
+	end
+	local sg=Duel.GetMatchingGroup(Card.IsCode,tp,LOCATION_DECK,0,nil,21159309):RandomSelect(tp,1)
+	local tc=sg:GetFirst()
+	if tc then
+		local e0=Effect.CreateEffect(e:GetHandler())
+		e0:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+		e0:SetCode(EVENT_FREE_CHAIN)
+		e0:SetOperation(function () Duel.SetChainLimitTillChainEnd(aux.FALSE) end)
+		tc:RegisterEffect(e0)
+		Duel.MoveToField(tc,tp,tp,LOCATION_MZONE,POS_FACEUP,true)
+		e0:Reset()
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+	end
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(2<<32))
+end
---
+++ script/c380000336.lua
@@ -0,0 +1,48 @@
+--Majestic Guardian
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	aux.AddSkillProcedure(c,1,false)
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_STARTUP)
+	e1:SetCountLimit(1)
+	e1:SetRange(0x5f)
+	e1:SetLabel(0)
+	e1:SetOperation(s.flipop)
+	c:RegisterEffect(e1)
+end
+s.listed_names={7841112,67030233}
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if e:GetLabel()==0 then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+		e1:SetOperation(s.disop)
+		Duel.RegisterEffect(e1,tp)
+	end
+	e:SetLabel(1)
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+	local c=e:GetHandler()
+	if tc:IsSummonType(SUMMON_TYPE_SYNCHRO) and tc:IsCode(7841112,67030233) and ep==tp then
+		Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+		Duel.Hint(HINT_CARD,tp,id)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetTargetRange(1,0)
+		e1:SetValue(s.aclimit)
+		e1:SetReset(RESET_PHASE+PHASE_END,2)
+		Duel.RegisterEffect(e1,tp)
+		Duel.Hint(HINT_SKILL_FLIP,tp,id|(2<<32))
+	end
+end
+function s.aclimit(e,re,tp)
+	return re:IsHasCategory(CATEGORY_TODECK+CATEGORY_SPECIAL_SUMMON) and re:GetHandler():IsCode(7841112,67030233)
+end

b79b78ef056aacbffd886475b0ca698b43125fce
ryoken08 <nekushiro@gmail.com>
2021-06-18 18:38:45 -0500

    Added new Skill
    - Added "The Dragon Knight’s Path" Skill
    - Swap the artworks between "The Dragon Knight’s Path" and "Spiral Spear Strike"

---
+++ script/c380000367.lua
@@ -0,0 +1,76 @@
+--The Dragon Knight’s Path
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	aux.AddSkillProcedure(c,1,false)
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_STARTUP)
+	e1:SetCountLimit(1)
+	e1:SetRange(0x5f)
+	e1:SetLabel(0)
+	e1:SetCondition(s.condition)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0xbd,0x10cf}
+function s.exfilter(c)
+	return c:IsType(TYPE_MONSTER) and (c:IsSetCard(0xbd) or (c:IsRace(RACE_DRAGON) and c:IsLevelAbove(5)))
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	--condition
+	return Duel.IsExistingMatchingCard(s.exfilter,tp,LOCATION_DECK,0,8,nil)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if e:GetLabel()==0 then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_FREE_CHAIN)
+		e1:SetCountLimit(1)
+		e1:SetCondition(s.flipcon)
+		e1:SetOperation(s.flipop)
+		Duel.RegisterEffect(e1,tp)
+	end
+	e:SetLabel(1)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--opd check
+	if Duel.GetFlagEffect(ep,id)>0 then return end
+	--condition
+	return aux.CanActivateSkill(tp)
+	and Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_HAND,0,1,nil)
+	and Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_DECK,0,1,nil,2106266)
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	--opd register
+	Duel.RegisterFlagEffect(ep,id,0,0,0)
+	local c=e:GetHandler()
+	--Register Special Summon Limit
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_FIELD)
+	e0:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e0:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e0:SetTargetRange(1,0)
+	e0:SetTarget(s.splimit)
+	e0:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e0,tp)
+	aux.RegisterClientHint(c,EFFECT_FLAG_OATH,tp,1,0,aux.Stringid(id,1),nil)
+	--return 1 card from your hand to your Deck
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_HAND,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_RULE)
+	end
+	--place 1 Galloping Gaia face-up on your field
+	local tc=Duel.GetFirstMatchingCard(Card.IsCode,tp,LOCATION_DECK,0,nil,2106266)
+	if tc then
+		Duel.MoveToField(tc,tp,tp,LOCATION_FZONE,POS_FACEUP,true)
+	end
+end
+function s.splimit(e,c)
+	return not ((c:IsType(TYPE_FUSION) and aux.IsMaterialListSetCard(c,0xbd)) or c:IsSetCard(0xbd) or c:IsSetCard(0x10cf) or (c:IsRace(RACE_DRAGON) and c:IsLevelAbove(5)))
+end

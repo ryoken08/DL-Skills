2b00642042cd4fdcc97472e2e94571d8b5e43385
ryoken08 <nekushiro@gmail.com>
2021-06-21 21:05:48 -0500

    Added new Skill
    Added "Ready to Get Defeated?"

---
+++ script/c380000314.lua
@@ -0,0 +1,35 @@
+--Ready to Get Defeated?
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)
+end
+s.listed_series={0x107b,0x48}
+function s.filter(c)
+	return c:IsFaceup() and c:IsSetCard(0x107b) and c:IsType(TYPE_XYZ) and c:IsStatus(STATUS_SPSUMMON_TURN) and not c:IsSetCard(0x48)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--opd check
+	if Duel.GetFlagEffect(ep,id)>0 then return end
+	--condition
+	return aux.CanActivateSkill(tp)
+	and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	--opd register
+	Duel.RegisterFlagEffect(ep,id,0,0,0)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.HintSelection(g)
+	local tc=g:GetFirst()
+	if tc then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetValue(1000)
+		tc:RegisterEffect(e1)
+	end
+end

9996a9fa6b0f88e3960e3f1a7a6fae06adaab6b8
ryoken08 <nekushiro@gmail.com>
2021-06-29 15:42:19 -0500

    Added "Halved Synchro"

---
+++ script/c380000342.lua
@@ -0,0 +1,78 @@
+--Halved Synchro
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	aux.AddSkillProcedure(c,1,false)
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_STARTUP)
+	e1:SetCountLimit(1)
+	e1:SetRange(0x5f)
+	e1:SetLabel(0)
+	e1:SetCondition(s.condition)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0x27}
+function s.exfilter(c)
+	return c:IsType(TYPE_MONSTER) and not c:IsSetCard(0x27)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	--condition
+	return not Duel.IsExistingMatchingCard(s.exfilter,tp,LOCATION_DECK,0,1,nil)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if e:GetLabel()==0 then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_FREE_CHAIN)
+		e1:SetCountLimit(1)
+		e1:SetCondition(s.flipcon)
+		e1:SetOperation(s.flipop)
+		Duel.RegisterEffect(e1,tp)
+	end
+	e:SetLabel(1)
+end
+function s.filter(c,tp)
+	return c:IsType(TYPE_SYNCHRO) and c:IsSetCard(0x27) and c:IsType(TYPE_TUNER)
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,tp,c:GetLevel()/2)
+end
+function s.spfilter(c,tp,lv)
+	return c:IsSetCard(0x27) and c:IsType(TYPE_SYNCHRO) and c:GetLevel()<lv
+		and Duel.GetLocationCountFromEx(tp,tp,nil,c)>0
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--opd check
+	if Duel.GetFlagEffect(ep,id)>0 then return end
+	--condition
+	return aux.CanActivateSkill(tp)
+	and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil,tp)
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	--opd register
+	Duel.RegisterFlagEffect(ep,id,0,0,0)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil,tp)
+	local g1=g:GetFirst()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+	local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,tp,g1:GetLevel()/2)
+	local tc=sg:GetFirst()
+	if tc then
+		local e0=Effect.CreateEffect(e:GetHandler())
+		e0:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+		e0:SetCode(EVENT_FREE_CHAIN)
+		e0:SetOperation(function () Duel.SetChainLimitTillChainEnd(aux.FALSE) end)
+		tc:RegisterEffect(e0)
+		Duel.MoveToField(tc,tp,tp,LOCATION_MZONE,POS_FACEUP,true)
+		e0:Reset()
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+	end
+end
